5b8155e7fac9c38aff9f6f6890e228fe93af8f2a
nsxv <189486048+nsxv-dev@users.noreply.github.com>
2025-09-28 17:20:27 +0200

    cleanup

--- project-x/WeatherForecast.cs
+++ project-x/WeatherForecast.cs
@@ -9,7 +9,5 @@ namespace project_x
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
-        // Test
-        // Test from branch
     }
 }
