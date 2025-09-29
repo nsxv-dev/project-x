0a585166d5e492fb6238e7bb9096819a524e9f2a
Mikolaj Matuszczyk <mikolaj.mat88@gmail.com>
2025-09-29 19:25:35 +0200

    Added registration service and controller

---
+++ project-x/Controllers/AuthController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using project_x.Data;
+using project_x.DTOs;
+using project_x.Models;
+using project_x.Services;
+
+namespace project_x.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly AuthService _authService;
+
+        public AuthController(AuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterUserDto registerUserDto)
+        {
+
+            if (string.IsNullOrEmpty(registerUserDto.Username) || string.IsNullOrEmpty(registerUserDto.Password))
+            {
+                return BadRequest("Username and password are required");
+            }
+
+            try
+            {
+                await _authService.RegisterUserAsync(registerUserDto.Username, registerUserDto.Password, registerUserDto.Email);
+                return Ok($"User {registerUserDto.Username} has been registered successfully");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
+
---
+++ project-x/Services/AuthService.cs
@@ -0,0 +1,38 @@
+﻿using project_x.Data;
+using project_x.Models;
+using BCrypt.Net;
+
+namespace project_x.Services
+
+{
+    public class AuthService
+    {
+        private readonly AppDbContext _context;
+
+        public AuthService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task RegisterUserAsync(string username, string password, string email)
+        {
+            if (await _context.Users.AnyAsync(u => u.Username == username))
+            {
+                throw new Exception("Username arleady exists");
+            }
+
+            var passwordHash = BCrypt.HashPassword(password);
+
+
+            var user = new User
+            {
+                Email = email,
+                Username = username,
+                PasswordHash = passwordHash
+            };
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
